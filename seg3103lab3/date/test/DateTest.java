cfc32d3c7e4bf799c599ddaa11e077c0aa136f93
bahmed24 <84537265+bahmed24@users.noreply.github.com>
2021-07-12 20:02:10 -0400

    Initial commit for Lab3: Added computation+tests and Date+tests

---
+++ seg3103lab3/date/test/DateTest.java
@@ -0,0 +1,84 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DateTest {
+
+
+
+  @Test
+  void nextDate_tc11() {
+    Date today = new Date(3453, 1, 30);
+    Date expectedTomorrow = new Date(3453, 1, 31);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc12() {
+    Date today = new Date(3456, 3, 30);
+    Date expectedTomorrow = new Date(3456, 3, 31);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc13() {
+    Date today = new Date(1901, 7, 31);
+    Date expectedTomorrow = new Date(1901, 8, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc14() {
+    Date today = new Date(3453, 1, 31);
+    Date expectedTomorrow = new Date(3453, 2, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_tc15() {
+    Date today = new Date(3456, 12, 31);
+    Date expectedTomorrow = new Date(3457, 1, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+  @Test
+  void nextDate_invalid_tc16() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1500, 2, 31)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc17() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1500, 2, 29)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc18() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(-1, 10, 20)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc19() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1458, 15, 12)
+    );
+  }
+
+  @Test
+  void nextDate_invalid_tc20() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1975, 6, -50)
+    );
+  }
+
+}
