50a71159726aebf931501688a7745279fbbd6dd4
bahmed24 <bashierahmed@cmail.carleton.ca>
2021-07-17 22:59:24 -0400

    Added failed_to_participate helper(refactoring)

--- grades/lib/grades/calculator.ex
+++ grades/lib/grades/calculator.ex
@@ -19,7 +19,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if (failed_to_participate(%{avg_labs: avg_labs, avg_exams:avg_exams, num_labs:num_labs})) do
       "EIN"
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
@@ -52,7 +52,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if (failed_to_participate(%{avg_labs: avg_labs, avg_exams:avg_exams, num_labs:num_labs})) do
       0
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
@@ -80,4 +80,9 @@ defmodule Grades.Calculator do
       Enum.sum(assignments) / Enum.count(assignments)
     end
   end
+
+  def failed_to_participate(%{avg_homework:avg_homework, avg_exams:avg_exam, num_labs:num_labs}) do
+    avg_homework < 0.4 || avg_exams < 0.4 || num_labs(labs) < 3
+  end
+
 end
