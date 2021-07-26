c20c785badb3ecf6402acb3d2e2cdb00cd304782
bahmed24 <bashierahmed@cmail.carleton.ca>
2021-07-25 23:55:56 -0400

    Added readme, screenshots and stubs for grades

---
+++ seg3103lab5/README.md
@@ -0,0 +1,16 @@
+# Lab 05
+
+| Outline | Value |
+| --- | --- |
+| Course | SEG 3X03 |
+| Date | Summer 2021 |
+| Professor | Andrew Forward, aforward@uottawa.ca |
+| TA | James Url, jamesurl@uottawa.ca |
+| Team | Andrew Forward 1484511<br>Ayana Forward 9021000 |
+
+## Grades compiling and running
+![grades compiling](assets/grades-compiling.png)
+![grades code running](assets/grades-running.png)
+
+## Grades app after Stub
+![grades app with stub](assets/grades-stubed.png)
--- seg3103lab5/grades/lib/grades/calculator.ex
+++ seg3103lab5/grades/lib/grades/calculator.ex
@@ -1,12 +1,21 @@
 defmodule Grades.Calculator do
+  # percentage_grade Stub
+  #
+  # returns a random percentage value
   def percentage_grade(marks) do
     :rand.uniform(100)
   end
 
+  # letter_grade Stub
+  #
+  # returns a random letter grade from the list below
   def letter_grade(marks) do
     Enum.random(["A+", "A", "A-","B+","B","C+","C","D+","D","EIN","F"])
     end
 
+  # numeric_grade Stub
+  #
+  # returns a random numeric value between 0 and 10
   def numeric_grade(marks) do
     :rand.uniform(10)
   end
